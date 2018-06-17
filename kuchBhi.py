11db1ad387e3b47a07de465889777c0c48f6be74
atiwari3bu <atiwari3@binghamton.edu>
2018-06-16 20:15:06 -0400

    .

--- kuchBhi.py
+++ kuchBhi.py
@@ -1,44 +1,26 @@
 # Plotting the map  
+import scipy
+import matplotlib.pyplot as plt
+from scipy import ndimage
+lats=[]
+longs=[]
 
-import turtle
-
-def setUpAxises(window,darty):
-    window.bgcolor("white")
-    darty.penup()
-    darty.pensize(3)
-    darty.pendown()
-    darty.goto(0,100)
-    darty.goto(0,-100)
-    darty.goto(0,0)
-    darty.goto(-100,0)
-    darty.goto(100,0)
-    darty.goto(0,0)
-    #darty.goto(42.08687349,-75.96634084)
-
-def movingTurtleThroughPoints(darty,x_cordinate,y_cordinate):
-    print(float(x_cordinate),float(y_cordinate))
-
-def plottingPoints(darty,lines):
+def plottingPoints(lines):
     for line in lines:
         if("lat" in line):  # Data Cleaning
             continue
         line=line.rstrip('\n')
         line=line.split(',')
-        x_cordinate,y_cordinate=line[0],line[1]
-        movingTurtleThroughPoints(darty,x_cordinate,y_cordinate)
+        lats.append(float(line[0]))
+        longs.append(float(line[1]))
+   
+    my_plot=plt.plot(lats,longs)
+    plt.show()
 
 def main():
-    window=turtle.Screen()
-    darty=turtle.Turtle()
-    darty.speed(0) # 0 is maximum speed
-    setUpAxises(window,darty)
-    darty.pensize(1)
-    
-    fileref=open("data.csv","r")  # Reading Lines from data
+    fileref=open("UDC2.csv","r")  # Reading Lines from data
     lines=fileref.readlines()
     fileref.close()
-
-    plottingPoints(darty,lines)
-    window.exitonclick() # exits on clicking on screen
+    plottingPoints(lines)
 
 main()
